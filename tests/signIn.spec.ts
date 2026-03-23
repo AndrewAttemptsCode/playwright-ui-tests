efff75478d9f06cfa1a6b658c122c3c814ef498e
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2026-03-23 16:10:23 +0000

    feat: add test to locate email field based off element narrowing locator

--- tests/signIn.spec.ts
+++ tests/signIn.spec.ts
@@ -10,11 +10,21 @@ test.beforeEach(async ({ page }) => {
 test("Log in", async ({ page }) => {
   await page.locator("#inputEmail1").fill("test@test.com");
   await page.locator("#inputPassword2").fill("password");
-  const radioButton = page.locator('label').filter({ hasText: /option 1/i });
+  const radioButton = page.locator("label").filter({ hasText: /option 1/i });
   await radioButton.click();
   await expect(radioButton).toBeChecked();
   await page
     .locator('button[status="primary"]')
     .filter({ hasText: /sign in/i })
     .click();
+});
+
+test("Locate card email input via elements", async ({ page }) => {
+  const emailInput = page
+    .locator("nb-card")
+    .filter({ hasText: /using the grid/i })
+    .getByLabel("email");
+
+  await emailInput.fill("email@email.com");
+  await expect(emailInput).toHaveValue("email@email.com");
 });
