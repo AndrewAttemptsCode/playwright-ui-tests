dd8b12b239fa966c7c41002cf135f05dd905c7d1
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2026-03-25 18:15:55 +0000

    feat: add test for radio button checks

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -10,8 +10,10 @@ test.describe("Form layouts page", () => {
     await page.getByRole("link", { name: /form layouts/i }).click();
   });
 
-  test("Input fields", async({ page }) => {
-    const loginForm = page.locator("nb-card").filter({ hasText: /using the grid/i });
+  test("Input fields", async ({ page }) => {
+    const loginForm = page
+      .locator("nb-card")
+      .filter({ hasText: /using the grid/i });
     const emailField = loginForm.getByRole("textbox", { name: "Email" });
     // Fill email input field with value
     await emailField.fill("test@test.com");
@@ -24,4 +26,26 @@ test.describe("Form layouts page", () => {
     await expect(emailField).toHaveValue("test@test.com");
   });
 
+  test("Radio buttons", async ({ page }) => {
+    const loginForm = page
+      .locator("nb-card")
+      .filter({ hasText: /using the grid/i });
+    
+    // Check radio option 1
+    const radioButton1 = loginForm.getByRole("radio", { name: /option 1/i });
+    await radioButton1.check({ force: true });
+
+    // Validate radio option 1 is checked
+    await expect(radioButton1).toBeChecked();
+
+    // Check radio option 2
+    const radioButton2 = loginForm.getByRole("radio", { name: /option 2/i });
+    await radioButton2.check({ force: true });
+
+    // Validate radio option 2 is checked
+    await expect(radioButton2).toBeChecked();
+
+    // Validate radio option 1 is no longer checked
+    await expect(radioButton1).not.toBeChecked();
+  });
 });
