66811a1f7661c304107fdbbab5764d090bde3615
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2026-04-02 20:04:38 +0100

    test: inline login form

--- tests/formLayouts.spec.ts
+++ tests/formLayouts.spec.ts
@@ -13,3 +13,12 @@ test("'Using the grid' login", async ({ page }) => {
   const form = new FormLayoutsPage(page);
   await form.signinGrid("email@email.com", "password123", "Option 1");
 });
+
+test("'Inline form' login", async ({ page }) => {
+  const navigate = new SideNav(page);
+  await navigate.navFormLayouts();
+
+  const form = new FormLayoutsPage(page);
+  await form.signinInline("Andrew", "test@test.com", true);
+  await form.signinInline("Bob", "email@email.com", false);
+});
