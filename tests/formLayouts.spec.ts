10a0df25245227b6323305fd45e41540ba9d0d8f
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2026-04-11 20:17:58 +0100

    feat: update inline test to iterate over 10 random user generated names and email from faker-js library

--- tests/formLayouts.spec.ts
+++ tests/formLayouts.spec.ts
@@ -1,5 +1,6 @@
 import { test } from "@playwright/test";
 import PageManager from "../pageObjects/PageManager";
+import { faker } from "@faker-js/faker";
 
 
 test.beforeEach(async ({ page }) => {
@@ -17,6 +18,11 @@ test("'Inline form' login", async ({ page }) => {
   const pm = new PageManager(page);
   await pm.navigateTo().formLayoutsPage();
 
-  await pm.onFormLayoutsPage().signinInline("Andrew", "test@test.com", true);
-  await pm.onFormLayoutsPage().signinInline("Bob", "email@email.com", false);
+  for (let i = 0; i <= 10; i++) {
+    const firstName = faker.person.firstName();
+    const lastName = faker.person.lastName();
+    const fullName = `${firstName} ${lastName}`;
+    const userEmail = `${firstName.toLowerCase()}.${lastName.toLowerCase()}${faker.number.int(1000)}@test.com`;
+    await pm.onFormLayoutsPage().signinInline(fullName, userEmail, true);
+  }
 });
